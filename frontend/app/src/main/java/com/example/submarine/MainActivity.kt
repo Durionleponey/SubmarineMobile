3b3460e739633632cb3f12b240020389441725c2
raimanta215 <r.macedon@students.ephec.be>
2025-11-12 00:07:52 +0100

    home screen page creation
    mise en place du'n page home pour la connexion

--- frontend/app/src/main/java/com/example/submarine/MainActivity.kt
+++ frontend/app/src/main/java/com/example/submarine/MainActivity.kt
@@ -5,19 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.submarine.auth.LoginActivity
+import com.example.submarine.auth.Signup
 import com.example.submarine.contacts.ContactsActivity
 import com.example.submarine.conversation.ConversationActivity
 import com.example.submarine.ui.theme.SubmarineTheme
@@ -50,14 +45,12 @@ fun MainScreen() {
         ) {
             Text(text = "Submarine ⚠️⚠️⚠️")
 
-
             Button(onClick = {
-                val intent = Intent(context, LoginActivity::class.java)
+                val intent = Intent(context, Signup::class.java)
                 context.startActivity(intent)
             }) {
-                Text("Login Account")
+                Text("go")
             }
-
             Button(onClick = {
                 val intent = Intent(context, ConversationActivity ::class.java)
                 context.startActivity(intent)

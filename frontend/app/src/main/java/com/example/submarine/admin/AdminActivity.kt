fb5d4fce1ad05c6434fa8d95358e30e225ba8694
Marry3005 <m.pierrard@students.ephec.be>
2025-11-12 14:28:32 +0100

    a corriger

--- frontend/app/src/main/java/com/example/submarine/admin/AdminActivity.kt
+++ frontend/app/src/main/java/com/example/submarine/admin/AdminActivity.kt
@@ -14,5 +14,27 @@ class AdminActivity : ComponentActivity() {
                 AdminNavigation()
             }
         }
+/**   
+import androidx.activity.compose.setContent
+import com.example.submarine.ui.theme.SubmarineTheme
+
+internal class {
+    internal inner class AdminActivity
+
+    fun ComponentActivity() {
+        val `fun`: override?
+        onCreate(savedInstanceState)
+        if (Bundle)
+            run {
+                super.onCreate(savedInstanceState)
+                setContent
+                run {
+                    SubmarineTheme
+                    run {
+                        TableauDeBordScreen()
+                    }
+                }
+            }
     }
+    **/
 }

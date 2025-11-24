582a7fd68bafe55cba7290cd0d767ca578d80ce8
Marry3005 <m.pierrard@students.ephec.be>
2025-11-24 16:48:00 +0100

    correction faite

--- frontend/app/src/main/java/com/example/submarine/admin/AdminActivity.kt
+++ frontend/app/src/main/java/com/example/submarine/admin/AdminActivity.kt
@@ -1,24 +1,19 @@
 package com.example.submarine.admin
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.submarine.ui.theme.SubmarineTheme
 
-internal class {
-    internal inner class AdminActivity
+class AdminActivity : ComponentActivity() {
 
-    fun ComponentActivity() {
-        val `fun`: override?
-        onCreate(savedInstanceState)
-        if (Bundle)
-            run {
-                super.onCreate(savedInstanceState)
-                setContent
-                run {
-                    SubmarineTheme
-                    run {
-                        TableauDeBordScreen()
-                    }
-                }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SubmarineTheme {
+                TableauDeBordScreen()
             }
+        }
     }
 }
